6f01ae2e18e1f605012212cf47ca83c95d60e91a
Abd0-taj <nrdntj@gmail.com>
2022-04-07 13:24:36 -0700

    documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * this is a function which prints a string
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is as piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
