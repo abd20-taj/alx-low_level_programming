a9e0e91050ab5eb872be375fa9fb3ff85d8f6de5
Abd0-taj <nrdntj@gmail.com>
2022-04-07 12:29:10 -0700

    betty check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-        char mystring[] = "Programming is like building a multilingual puzzle\n";
-        puts(mystring);
-	return (0);
+char mystring[] = "Programming is like building a multilingual puzzle\n";
+puts(mystring);
+return (0);
 }
