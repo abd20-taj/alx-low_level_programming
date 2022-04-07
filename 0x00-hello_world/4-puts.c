f15f8c464991ad27dfaae361e4e9a4cf26122181
Abd0-taj <nrdntj@gmail.com>
2022-04-07 07:49:32 -0700

    printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char mystring[] = "Programming is like building a multilingual puzzle\n";
+        puts(mystring);
+	return (0);
+}
