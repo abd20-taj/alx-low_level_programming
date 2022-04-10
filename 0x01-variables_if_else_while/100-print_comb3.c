288d0bc36cf7db4523f25de433a9f91aa473e005
Abd0-taj <nrdntj@gmail.com>
2022-04-10 04:09:50 -0700

    add file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print combination of numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int numb;
+int number;
+
+while (number < 10)
+{
+numb = 0;
+while (numb < 10)
+{
+if (number != numb && number < numb)
+{
+putchar('0' + number);
+putchar('0' + numb);
+
+if (numb + number != 17)
+{putchar(',');
+putchar(',');
+}
+}
+numb++;
+}
+number++;
+}
+putchar('\n');
+return (0);
+}
