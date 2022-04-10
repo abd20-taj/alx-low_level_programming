bde5d207aa5710fab4bc403a5e793a2acb16e26d
Abd0-taj <nrdntj@gmail.com>
2022-04-10 04:34:49 -0700

    fix bug

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,14 +16,15 @@ while (var2 < 10)
 {
 var1 = 0;
 while (var1 < 10)
-{if (var1 != var2 && var2 != var3 && var3 < var2 && var2 < var1)
+{
+if (var1 != var2 && var2 != var3 && var3 < var2 && var2 < var1)
 {putchar('0' + var3);
 putchar('0' + var2);
 putchar('0' + var1);
 
 if (var1 + var2 + var3 != 9 + 8 + 7)
 {putchar(',');
- putchar(' ');
+putchar(' ');
 }
 }
 var1++;
@@ -31,6 +32,7 @@ var1++;
 var2++;
 }
 var3++;
-}putchar('\n');
+}
+putchar('\n');
 return (0);
 }
