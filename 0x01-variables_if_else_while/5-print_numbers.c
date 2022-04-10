7765f1f81ecc8f804c09ae8efa3ac8bcdaf8e3f3
Abd0-taj <nrdntj@gmail.com>
2022-04-10 00:42:00 -0700

    add file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+int numb;
+
+for (numb = 0; numb < 10; numb++)
+{
+printf("%d", numb);
+}
+printf("\n");
+return (0);
+}
