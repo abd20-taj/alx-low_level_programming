f21b468e3bc393783cdfd723aa3731d9318c6f1b
Abd0-taj <nrdntj@gmail.com>
2022-04-12 04:18:58 -0700

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10x
+ *
+ * Return: has no retun value
+ */
+void print_alphabet_x10(void)
+{
+	int alph = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		_putchar('\n');
+	}
+}
