78de3f5cf5a1b42cd1eb6b6b398f11f1f1025a81
Abd0-taj <nrdntj@gmail.com>
2022-04-13 00:55:19 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,16 +6,23 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1); }
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0); }
-	else
-	{
-		_putchar('-');
-		return (-1); }
+	int ret;
+
+if (n > 0)
+{
+	ret = 1;
+	_putchar('+');
+}
+else if (n == 0)
+{
+	ret = 0;
+	_putchar('0');
+}
+else
+{
+	ret = -1;
+	_putchar('-');
+}
+
+return (ret);
 }
