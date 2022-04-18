00ef12784c9c0262908a1b00649f19db405d9768
Abd0-taj <nrdntj@gmail.com>
2022-04-18 07:04:12 -0700

    add

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints some characters of a string
+ * @str: string to be manipulated
+ * Return: doesn't have any
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if ((str[i] % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+			continue;
+	}
+	_putchar('\n');
+
+
+}
