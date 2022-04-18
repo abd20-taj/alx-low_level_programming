b49f61296f365d633469e9b90177a2abd0112088
Abd0-taj <nrdntj@gmail.com>
2022-04-18 05:00:32 -0700

    add

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: void
+ *
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		_putchar('str[i]');
+	}
+	_putchar('\n');
+}
